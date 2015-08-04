3447e3d8c111e2473e30047b0424c39b10b9b3a4
Alex Leonetti <leonetti.tech@gmail.com>
2015-08-04 11:55:22 -0700

    Choice selection of events works

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -3,6 +3,15 @@ var app = angular.module('localsOnly', []);
 app.controller('InputController', ['$scope', '$http', function($scope, $http) {
 	$scope.data = {};
 	$scope.city = 'in your city';
+	$scope.restaurants = true;
+	$scope.clubs = true;
+	$scope.bars = true;
+	$scope.stores = true;
+	$scope.parks = true;
+	$scope.tourists = true;
+	$scope.yoga = true;
+
+
 	$scope.sendLocation = function(location) {
 		$scope.city = location;
 		$http.get('/location/' + location)
--- server.js
+++ server.js
@@ -17,34 +17,49 @@ app.get('/location/:location', function (req, res) {
 
   // stores each get request from yelp in a response object
   var response = {};
-  var mapRequests = function(term,businessArray){
+  var mapRequests = function(term, businessArray){
     // adds each returned response from each get request into response
     response[term] = businessArray;
 
     // if all get requests finish then it will send the response object as a result
-    if(response.restaurants&&response.club&&response.store&&response.bar){
+    if(response.restaurants&&response.club&&response.store&&response.bar&&response.parks&&response.tourists&&response.yoga){
       res.json(response);
     }
   }
 
   yelp.search({term: "restaurants", location: location, sort:2, limit:4,radius_filter:10000}, function(error, data) {
     if(error) res.end("Error");
-    mapRequests("restaurants",data.businesses);
+    mapRequests("restaurants", data.businesses);
   });
 
   yelp.search({term: "dance clubs", location: location, sort:2, limit:2,radius_filter:10000}, function(error, data) {
     if(error) res.end("Error");
-    mapRequests("club",data.businesses);
+    mapRequests("club", data.businesses);
   });
 
   yelp.search({term: "stores", location: location, sort:2, limit:2,radius_filter:10000}, function(error, data) {
     if(error) res.end("Error");
-    mapRequests("store",data.businesses);
+    mapRequests("store", data.businesses);
   });
 
   yelp.search({term: "pubs", location: location, sort:2, limit:4,radius_filter:10000}, function(error, data) {
     if(error) res.end("Error");
-    mapRequests("bar",data.businesses);
+    mapRequests("bar", data.businesses);
+  });
+
+  yelp.search({term: "parks", location: location, sort:2, limit:2,radius_filter:10000}, function(error, data) {
+    if(error) res.end("Error");
+    mapRequests("parks", data.businesses);
+  });
+
+  yelp.search({term: "tourist attractions", location: location, sort:2, limit:4,radius_filter:10000}, function(error, data) {
+    if(error) res.end("Error");
+    mapRequests("tourists", data.businesses);
+  });
+
+  yelp.search({term: "yoga", location: location, sort:2, limit:2,radius_filter:10000}, function(error, data) {
+    if(error) res.end("Error");
+    mapRequests("yoga", data.businesses);
   });
 });
 
